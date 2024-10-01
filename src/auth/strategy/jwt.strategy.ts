e689fbc25b0f307239fe918c680aebbeccc8d252
phong28zk <lamperougelelouchanm@gmail.com>
2024-10-01 11:43:16 +0700

    feat/RBAC: role, modify jwt strategy and user entity

---
+++ src/auth/strategy/jwt.strategy.ts
@@ -0,0 +1,32 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy, ExtractJwt } from 'passport-jwt';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from 'src/entities/user.entity';
+import { request } from 'express';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: process.env.JWT_SECRET,
+    });
+  }
+
+  async validate(payload): Promise<User> {
+    const { id } = payload;
+    const user = await this.usersRepository.findOneBy(id);
+    // console.log('This is the payload:\n --------------------------------------------------------');
+    // console.log('user', user);
+    if (!user) {
+      throw new UnauthorizedException('Login first to access this endpoint.');
+    }
+    request.user = user;
+    return user;
+  }
+}
