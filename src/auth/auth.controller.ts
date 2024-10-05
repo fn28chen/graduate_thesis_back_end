56f14d369f8d436fac448e8957b10b8324bf6ea3
phong28zk <lamperougelelouchanm@gmail.com>
2024-10-05 17:32:14 +0700

    fix/hotfix: prettier fix

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,10 +1,11 @@
-import { Body, Controller, Post, Req } from '@nestjs/common';
+import { Body, Controller, Post, UsePipes } from '@nestjs/common';
 import {
-  Body,
-  Controller,
-  Post,
-  UsePipes,
-} from '@nestjs/common';
+  ApiBody,
+  ApiConsumes,
+  ApiOperation,
+  ApiTags,
+  getSchemaPath,
+} from '@nestjs/swagger';
 
 import { AuthService } from './auth.service';
 import { ValidationPipe } from 'src/utils/validation/validation.pipe';
@@ -13,17 +14,41 @@ import { SignUpDto } from './dto/signup.dto';
 import { LoginDto } from './dto/login.dto';
 import { LogoutDto } from './dto/logout.dto';
 
+@ApiTags('auth')
 @Controller('auth')
 @UsePipes(new ValidationPipe())
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('signup')
+  @ApiOperation({ summary: 'Sign up' })
+  @ApiConsumes('application/json')
+  @ApiBody({
+    schema: {
+      type: 'object',
+      example: {
+        username: 'Phong',
+        email: 'phong123@gmail.com',
+        password: '123456@a',
+      },
+    },
+  })
   async signUp(@Body() signUpDto: SignUpDto) {
     return this.authService.signUp(signUpDto);
   }
 
   @Post('login')
+  @ApiOperation({ summary: 'Log in' })
+  @ApiConsumes('application/json')
+  @ApiBody({
+    schema: {
+      type: 'object',
+      example: {
+        email: 'phong123@gmail.com',
+        password: '123456@a',
+      },
+    },
+  })
   async login(@Body() loginDto: LoginDto) {
     return this.authService.login(loginDto);
   }
@@ -33,8 +58,18 @@ export class AuthController {
     return this.authService.refreshTokens(refreshToken.refreshToken);
   }
 
-  
   @Post('logout')
+  @ApiOperation({ summary: 'Log out' })
+  @ApiConsumes('application/json')
+  @ApiBody({
+    schema: {
+      type: 'object',
+      example: {
+        accessToken: 'accessToken',
+        refreshToken: 'refreshToken',
+      },
+    },
+  })
   async logout(@Body() logoutDto: LogoutDto) {
     return this.authService.logout(logoutDto);
   }
