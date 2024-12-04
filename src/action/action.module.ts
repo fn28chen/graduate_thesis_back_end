fa6b619ffbeab17db7b9be55e6502677e3d8f8c7
phong28zk <lamperougelelouchanm@gmail.com>
2024-12-04 10:47:08 +0700

    fix/bug: fix bug

--- src/action/action.module.ts
+++ src/action/action.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ActionController } from './action.controller';
 import { ActionService } from './action.service';
+import { JwtService } from '@nestjs/jwt';
+import { KeyTokenService } from 'src/key-token/key-token.service';
+import { KeyTokenModule } from 'src/key-token/key-token.module';
 
 @Module({
   controllers: [ActionController],
-  providers: [ActionService],
+  imports: [KeyTokenModule],
+  providers: [ActionService, JwtService],
 })
 export class ActionModule {}
